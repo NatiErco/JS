6635cab2d8adfe82dd15d754c0600844d9da09f0
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-05-03 23:00:08 +0200

    Add files via upload

---
+++ cierres2.js
@@ -0,0 +1,13 @@
+function printName() {
+  let helloName = "Hello John";
+
+  function inner() {
+    return helloName;
+  }
+
+  setTimeout(function () {
+    console.log(inner());
+  }, 1000);
+}
+
+printName();
