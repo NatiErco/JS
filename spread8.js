c842c284910d04c6385f92f16f8d34adab4f4cce
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-05-20 15:25:25 +0200

    Add files via upload

---
+++ spread8.js
@@ -0,0 +1,11 @@
+// Realice una refactorización de código utilizando el operador de extensión. La salida de console.log debe ser la misma.
+
+function sum(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+const numbers = [1, 2, 3];
+
+// console.log(sum(numbers[0], numbers[1], numbers[2]));
+
+console.log(sum(...numbers));
